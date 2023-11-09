8dd7c911d91c658c8c0672413e36dbfbaea99b25
hugocruzlfc <hugocruzlfc@gmail.com>
2023-11-09 15:46:47 +0100

    feat: adding fields validations

--- src/coffees/dto/create-coffee.input/create-coffee.input.ts
+++ src/coffees/dto/create-coffee.input/create-coffee.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 
 @InputType({ description: 'Input type for creating a coffee' })
 export class CreateCoffeeInput {
+  @MinLength(3)
   @Field(() => String, { description: 'A new coffee' })
   name: string;
   brand: string;
